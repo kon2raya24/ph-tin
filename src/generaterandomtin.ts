cdb8dc9a6770f9cd05e671f0cd33cbc61b3b4fc6
kon2raya24 <konturaya@gmail.com>
2026-06-22 09:02:58 +0000

    docs: add JSDoc comments to generaterandomtin.ts

--- src/generaterandomtin.ts
+++ src/generaterandomtin.ts
@@ -1,3 +1,6 @@
+/**
+ * generateRandomTIN
+ */
 export function generateRandomTIN(): string {
   const tin = Array.from({length: 12}, () => Math.floor(Math.random() * 10)).join("");
   return formatTIN(tin);
